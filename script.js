6d4d5c1e677b3d3045f572b1774d85df6fae3bc1
ciboq <90256806+ciboq@users.noreply.github.com>
2022-04-28 15:27:59 -0400

    m

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('keydown', logKey)
+
+function logKey(e){
+    switch(e.which){
+        case 65:
+           ('letterA.png').trigger('click');
+           break;
+    }
+}
+
+// window.addEventListener("keydown", checkKeyPress);
+// // The parameters of the event listener are event, function to run, useCapture (optional).
+// // This event listener runs checkKeyPress function when a key is pressed down
+// // You can use keydown or keyup to check what key has been pressed or released
+ 
+ 
+// // this function checks to see if the letter 'a' key has been pressed
+// function checkKeyPress(key) {
+//  if (key.keyCode == "65") {
+//  alert("The 'a' letter key has been pressed.");
+//  // 65 is the keycode that is returned when the 'a' letter key is pressed
+//  // if you use keypress event instead of keydown/up then you must use ASCII code instead of keycode
+//  }
+// }
